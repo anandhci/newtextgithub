0ee0b3f8e360c1448e5608d4647d12f2153bb541
Anand Pandiyan <apandiyan@gmail.com>
2023-06-22 16:17:10 +0400

    text changed,

--- textgithub/ContentView.swift
+++ textgithub/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, working??")
+            Text("Yes i think, it is working??")
         }
         .padding()
     }
